7cba99b58d50acb7654ce491c9ab56eb42c575eb
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-11 18:43:30 -0500

    Clase Asignatura - array

---
+++ Estructuras_app/Semana3.cs
@@ -0,0 +1,23 @@
+public class Asignatura
+{
+    private string nombre;
+    private double[] notas;
+
+    //genero el constructor
+    public Asignatura(string _nombre, double[] _notas)
+    {
+        nombre = _nombre;
+        notas = new double[3];
+        notas = _notas;
+    }
+
+    public string getAsignatura()
+    {
+        return nombre;
+    }
+
+    public double[] getNotas()
+    {
+        return notas;
+    }
+}
