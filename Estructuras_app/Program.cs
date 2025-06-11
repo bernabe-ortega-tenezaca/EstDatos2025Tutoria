a2978249f1a4fba5560e17e29024504284e1936b
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-11 18:33:16 -0500

    Clase cuadrado -area

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine("=============================");
 System.Console.WriteLine();
+
+Cuadrado mi_cuadrado = new Cuadrado(4);
+Console.WriteLine("El área del cuadrado es: " + mi_cuadrado.area());
---
+++ Estructuras_app/Semana2.cs
@@ -0,0 +1,14 @@
+public class Cuadrado
+{
+    private int lado;
+
+    public Cuadrado(int _lado)
+    {
+        lado = _lado;
+    }
+
+    public int area()
+    {
+        return lado * lado;
+    }
+}
