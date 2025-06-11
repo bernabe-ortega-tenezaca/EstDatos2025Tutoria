d99d85adb53a5a0b79e06e8e58227f789b1209f8
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-11 18:46:51 -0500

    Prueba de funcionamiento asignatura

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -4,3 +4,14 @@ System.Console.WriteLine();
 
 Cuadrado mi_cuadrado = new Cuadrado(4);
 Console.WriteLine("El área del cuadrado es: " + mi_cuadrado.area());
+System.Console.WriteLine();
+
+Asignatura matematicas = new Asignatura("matematicas", [12, 13, 18]);
+
+System.Console.WriteLine(matematicas.getAsignatura());
+System.Console.WriteLine(matematicas.getNotas());
+
+double[] minotas = matematicas.getNotas();
+System.Console.WriteLine(minotas[0]);
+System.Console.WriteLine(minotas[1]);
+System.Console.WriteLine(minotas[2]);
