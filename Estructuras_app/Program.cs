0269ab470e326ef59aadd5ba21c1a1c00da64b32
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-14 08:02:49 -0500

    Definición de array

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -1,17 +1,37 @@
-﻿Console.WriteLine("Universidad Estatal Amazónica");
+﻿using System.Net.WebSockets;
+
+Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine("=============================");
 System.Console.WriteLine();
 
-Cuadrado mi_cuadrado = new Cuadrado(4);
-Console.WriteLine("El área del cuadrado es: " + mi_cuadrado.area());
-System.Console.WriteLine();
+//usar cw + TAB
+System.Console.WriteLine("Clase encuentro - Arrays");
 
-Asignatura matematicas = new Asignatura("matematicas", [12, 13, 18]);
+//Definición de un array
+string[] estudiantes = new string[5];
 
-System.Console.WriteLine(matematicas.getAsignatura());
-System.Console.WriteLine(matematicas.getNotas());
 
-double[] minotas = matematicas.getNotas();
-System.Console.WriteLine(minotas[0]);
-System.Console.WriteLine(minotas[1]);
-System.Console.WriteLine(minotas[2]);
+
+
+
+
+
+
+
+
+
+
+
+// Cuadrado mi_cuadrado = new Cuadrado(4);
+// Console.WriteLine("El área del cuadrado es: " + mi_cuadrado.area());
+// System.Console.WriteLine();
+
+// Asignatura matematicas = new Asignatura("matematicas", [12, 13, 18]);
+
+// System.Console.WriteLine(matematicas.getAsignatura());
+// System.Console.WriteLine(matematicas.getNotas());
+
+// double[] minotas = matematicas.getNotas();
+// System.Console.WriteLine(minotas[0]);
+// System.Console.WriteLine(minotas[1]);
+// System.Console.WriteLine(minotas[2]);
