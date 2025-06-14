52681d53b7d44b4070e633a1b7d7cb8d3dca0af6
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-14 08:28:36 -0500

    definicion e inicializacion de arrays

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -17,8 +17,12 @@ for (int i = 0; i < 5; i++)
     System.Console.WriteLine("El elemento " + i + " es " + estudiantes[i]);
 }
 
-
-
+//ejemplo array inicializado directamente
+int[] numeros = { 10, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50, 20, 30, 40, 50 };
+for (int i = 0; i < numeros.Length; i++)
+{
+   System.Console.WriteLine(numeros[i]);
+}
 
 
 
