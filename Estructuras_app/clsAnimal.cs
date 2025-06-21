83afa81bd3396fff331951967675919c3f31aef9
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-21 07:54:53 -0500

    Uso de propiedades prop

--- Estructuras_app/clsAnimal.cs
+++ Estructuras_app/clsAnimal.cs
@@ -1,24 +1,37 @@
 public class Animal
 {
-    private string Nombre;
+    public string Nombre { get; set; }
+    public double Largo { get; set; }
+    public int Edad { get; set; }
 
-    //constructor
-
-    public Animal(string _nombre)
+    //Constructor
+    public Animal(string _nombre, double _largo, int _edad)
     {
         Nombre = _nombre;
-
+        Largo = _largo;
+        Edad = _edad;
     }
 
-    public void SetNombre(string _nombre)
-    {
-        Nombre = _nombre;
-    }
 
-    public string GetNombre()
-    {
-        return Nombre;
-    }
+    // private string Nombre;
+
+    // //constructor
+
+    // public Animal(string _nombre)
+    // {
+    //     Nombre = _nombre;
+
+    // }
+
+    // public void SetNombre(string _nombre)
+    // {
+    //     Nombre = _nombre;
+    // }
+
+    // public string GetNombre()
+    // {
+    //     return Nombre;
+    // }
 
 
 }
