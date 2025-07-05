9eae30a32727e589904f9ce79c26b40d349e251e
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-07-05 08:31:15 -0500

    Lista simpe insertar

--- Estructuras_app/ce-semana6/clsListaSimple.cs
+++ Estructuras_app/ce-semana6/clsListaSimple.cs
@@ -6,4 +6,33 @@ public class ListaSimple
     {
         head = null;
     }
+
+    public void InsertarInicio(string dato)
+    {
+        var nuevoNodo = new clsNodo(dato);
+
+        nuevoNodo.Siguiente = head;
+        head = nuevoNodo;
+    }
+
+    public void InsertarFinal(string dato)
+    {
+        clsNodo nuevoNodo = new clsNodo(dato);
+
+        if (head == null)
+        {
+            head = nuevoNodo;
+        }
+        else
+        {
+            clsNodo actual = head;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevoNodo;
+        }
+
+    }
+
 }
