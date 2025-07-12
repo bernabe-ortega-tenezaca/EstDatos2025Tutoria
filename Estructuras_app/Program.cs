fe8c3be7a7244354ce722ac65052b9f087309030
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-07-12 08:42:49 -0500

    limpieza - pilas

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -5,13 +5,3 @@ Console.WriteLine();
 //ceListas.run();
 //ceVectores.ejecutar();
 //Pilas.run();
-
-Stack<string> estudiantes = new Stack<string>();
-
-estudiantes.Push("Ana");
-estudiantes.Push("Carlos");
-Console.WriteLine("Ingrese un nombre");
-estudiantes.Push(Console.ReadLine());
-estudiantes.Push("Juan");
-
-Pilas.ImprimirPila(estudiantes);
