f424a8c867f5c6e21c1fe1c176c40d82e383ba2d
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-07-12 08:20:30 -0500

    organizacion codigo - pilas

---
+++ Estructuras_app/CEncuentros/ce-semana7/pilas.cs
@@ -0,0 +1,43 @@
+public class Pilas
+{
+    public static void run()
+    {
+        System.Console.WriteLine("Pilas");
+        System.Console.WriteLine("===============");
+
+        Stack<int> numeros = new Stack<int>();
+
+        numeros.Push(1);
+        numeros.Push(2);
+        numeros.Push(20);
+        numeros.Push(4);
+
+        //recorrer los elementos de la pila
+        foreach (int item in numeros)
+        {
+            System.Console.WriteLine(item);
+        }
+
+        int ultimo = numeros.Peek();
+
+        Console.WriteLine("el último elemento es: " + ultimo);
+        Console.WriteLine("último elemento {0} y el doble de último es {1}", ultimo, ultimo*2);
+        Console.WriteLine($"último elemento es: {ultimo}");
+
+        //Eliminar elementos
+        numeros.Pop();
+        int eliminado = numeros.Pop();
+        System.Console.WriteLine();
+
+
+        //recorrer los elementos de la pila
+        foreach (int item in numeros)
+        {
+            System.Console.WriteLine(item);
+        }
+
+        System.Console.WriteLine("elemento eliminado: " + eliminado);
+   
+    }
+
+}
--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -4,38 +4,4 @@ Console.WriteLine();
 
 //ceListas.run();
 //ceVectores.ejecutar();
-System.Console.WriteLine("Pilas");
-System.Console.WriteLine("===============");
-
-Stack<int> numeros = new Stack<int>();
-
-numeros.Push(1);
-numeros.Push(2);
-numeros.Push(20);
-numeros.Push(4);
-
-//recorrer los elementos de la pila
-foreach (int item in numeros)
-{
-    System.Console.WriteLine(item);
-}
-
-int ultimo = numeros.Peek();
-
-Console.WriteLine("el último elemento es: " + ultimo);
-Console.WriteLine("último elemento {0} y el doble de último es {1}", ultimo, ultimo*2);
-Console.WriteLine($"último elemento es: {ultimo}");
-
-//Eliminar elementos
-numeros.Pop();
-int eliminado = numeros.Pop();
-System.Console.WriteLine();
-
-
-//recorrer los elementos de la pila
-foreach (int item in numeros)
-{
-    System.Console.WriteLine(item);
-}
-
-System.Console.WriteLine("elemento eliminado: " + eliminado);
+Pilas.run();
