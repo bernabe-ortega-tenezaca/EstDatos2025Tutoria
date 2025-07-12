90ee9c7f58042974ff87d17044db23e45b811601
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-07-12 08:02:16 -0500

    Definicion de pila

--- Estructuras_app/Program.cs
+++ Estructuras_app/Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine();
 
 //ceListas.run();
 //ceVectores.ejecutar();
+System.Console.WriteLine("Pilas");
+System.Console.WriteLine("===============");
+
+Stack<int> numeros = new Stack<int>();
