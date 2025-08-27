759682acfd88974e310014b227d2db1fdb790c39
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-08-27 18:55:43 -0500

    Captura de exception

--- Estructuras_app/CEncuentros/ce-semana11/Diccionarios.cs
+++ Estructuras_app/CEncuentros/ce-semana11/Diccionarios.cs
@@ -2,7 +2,34 @@ public class Diccionarios
 {
     public static void run()
     {
-        System.Console.WriteLine("-- Diccionarios --");  
+        System.Console.WriteLine("-- Diccionarios --");
+
+        //Definimos un diccionario
+        Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+        estudiantes.Add(1, "Ana");
+
+        Console.Write("Ingrese una clave: ");
+        int clave = int.Parse(Console.ReadLine());
+
+        Console.Write("Nombre del estudiante: ");
+        string nombre = Console.ReadLine();
+
+
+
+        try
+        {
+            estudiantes.Add(clave, nombre);
+        }
+        catch (System.ArgumentException e)
+        {
+            System.Console.WriteLine("Ha ocurrido un error \n La clave ya existe!!");
+        }
+
+
+        System.Console.WriteLine(estudiantes[clave]);
+
+
     }
 
 }
