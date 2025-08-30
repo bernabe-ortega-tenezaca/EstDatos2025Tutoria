d5441ef7b3ca997603830c1c7981f1c7e1d7a025
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-08-30 08:07:54 -0500

    Lectura de datos para el diccionario

--- Estructuras_app/CEncuentros/ce-semana11/sabado.cs
+++ Estructuras_app/CEncuentros/ce-semana11/sabado.cs
@@ -2,11 +2,26 @@ public class ClaveValor
 {
     public static void run()
     {
+        // Definición de un diccionario
         Dictionary<int, string> estudiantes = new Dictionary<int, string>();
         estudiantes.Add(1, "Juan");
         estudiantes.Add(2, "Pedro");
         estudiantes.Add(3, "Maria");
         estudiantes.Add(4, "Anabel");
-    }
 
+        // Definición e inicialización de un diccionario
+        Dictionary<int, string> docentes = new()
+        {
+            {1, "Anita"}
+        };
+
+        System.Console.WriteLine(" -- Lista de estudiantes --");
+        System.Console.Write("Ingrese un código: ");
+        int codigo = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Ingres el nombre: ");
+        string nombre = Console.ReadLine();
+
+        estudiantes.Add(codigo, nombre);
+
+    }
 }
