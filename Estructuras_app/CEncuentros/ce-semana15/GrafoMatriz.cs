020b10931714983831ad1388a7a1adda1fc48136
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-10-01 17:39:14 -0500

    Grafo matriz

---
+++ Estructuras_app/CEncuentros/ce-semana15/GrafoMatriz.cs
@@ -0,0 +1,28 @@
+public class GrafoMatriz{
+
+    private int[,] adjMatrix; //Matriz de adyacencia es de VxV
+
+    //Constructor
+    public GrafoMatriz(int vertices){
+        adjMatrix = new int[vertices, vertices];//inicialización
+    }
+
+    // 0 es ausencia y 1 presencia
+    // ponderada -> peso, o nulo para indicar ausencia
+    public void AddEdge(int v, int w){
+        adjMatrix[v,w] = 1; //indica presencia
+    }
+
+    public void PrintGrafo(){
+        int vertices = adjMatrix.GetLength(0);
+
+        for (int i = 0; i < vertices; i++) //fila
+        {
+            for (int j = 0; j < vertices; j++) //columna
+            {
+                System.Console.Write($"{adjMatrix[i,j]} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
