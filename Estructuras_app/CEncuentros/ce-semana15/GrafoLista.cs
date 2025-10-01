7333699c940583aa84a80a48fd30f0be5d5c83cd
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-10-01 17:38:08 -0500

    Grafo lista de adyacencia

---
+++ Estructuras_app/CEncuentros/ce-semana15/GrafoLista.cs
@@ -0,0 +1,75 @@
+public class GrafoLista{
+    private int V; //cantidad de vértices
+    private List<int>[] adj; //lista de adyacencia
+
+    //Constructor
+    public GrafoLista(int vertices){
+        V = vertices;
+        adj = new List<int>[V];
+
+        for (int i = 0; i < V; i++)//una lista de enteros por cada vertice
+        {
+            adj[i] = new List<int>(); //creamos una lista para cada V
+        }
+    }
+
+    public void AddAristaGL(int v, int w){
+        adj[v].Add(w);
+    }
+
+    void BFS(int v){
+        Queue<int> queue = new Queue<int>();
+        bool[] visitado = new bool[v];
+
+        queue.Enqueue(v);
+        visitado[v] = true;
+
+        while (queue.Count > 0)
+        {
+            int u = queue.Dequeue();
+
+            foreach (int w in adj[u])
+            {
+                if(!visitado[w]){
+                    queue.Enqueue(w);
+                    visitado[w]= true;
+                }
+            }
+        }    
+    }
+
+    public void PrintGraph(){
+        for (int i = 0; i < V; i++)
+        {
+            System.Console.Write($"Vertice {i}: ");
+            foreach (var item in adj[i])
+            {
+                System.Console.Write($" -> {item}");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    public string PrintGraph(bool imprimir=true){
+        string msg = "No se puede imprimir el grafo";
+        try
+        {
+            for (int i = 0; i < V; i++)
+            {
+                System.Console.Write($"Vertice {i}: ");
+                foreach (var item in adj[i])
+                {
+                    System.Console.Write($" -> {item}");
+                }
+                System.Console.WriteLine();
+            }
+            msg = "\n"; 
+        }
+        catch (System.Exception e)
+        {
+            System.Console.WriteLine(e);
+        }
+        return msg;
+    }
+
+}
